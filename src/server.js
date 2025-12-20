0f2676b58ebb0432978e97b4f3b9cde91486e6d7
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-12-20 21:11:49 +0530

    Add basic server setup and project structure

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+// const PORT = 3000;
+
+app.listen(3000, () => {
+  console.log('DevTinder server started');
+});
+
+// app.listen(4000, () => {
+//   console.log('DevTinder 2nd server started');
+// });

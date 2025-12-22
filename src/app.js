62b770751022ae2ca0934158c928d5b5dec2971b
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-12-22 21:54:34 +0530

    Refactor route handlers for clarity and consistency

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to DevTinder');
+app.use('/hello/2', (req, res) => {
+  res.send('Hello from hello/2');
 });
 
 app.use('/hello', (req, res) => {
-  res.send('Hello hello hello');
+  res.send('Hello from hello');
+});
+
+app.use('/test', (req, res) => {
+  res.send('Hello from test');
+});
+
+app.use('/', (req, res) => {
+  res.send('Welcome to DevTinder');
 });
 
 module.exports = app;

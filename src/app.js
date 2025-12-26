a7400a6909f5251147d46e2714f4e1d9ef7b2185
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-12-26 12:06:33 +0530

    Playing with req objects and fixing auto-restart server bug

--- src/app.js
+++ src/app.js
@@ -2,26 +2,16 @@ const express = require('express');
 
 const app = express();
 
-// this will handle all HTTP methods for /user
-app.use('/user', (req, res) => {
-  res.send('Handling all HTTP methods for /user');
-});
-
-// this will only handle GET call to /user
 app.get('/user', (req, res) => {
-  res.send({ firstName: 'Niraj', lastName: 'Kumar' });
-});
-
-// this will only handle POST call to /user
-app.post('/user', (req, res) => {
-  // saving data to DB
-  res.send('data saved successfully to database');
-});
-
-// this will only handle DELETE call to /user
-app.delete('/user', (req, res) => {
-  // deleting data from DB
-  res.send('data deleted successfully from database');
+  console.log(req.method);        // GET, POST, etc.
+  console.log(req.url);           // /user
+  console.log(req.params);        // Route parameters
+  console.log(req.query);         // Query string parameters
+  console.log(req.body);          // Request body (needs middleware)
+  console.log(req.headers);       // HTTP headers
+  console.log(req.cookies);       // Cookies (needs cookie-parser)
+  console.log(req.ip);            // Client IP address
+  res.send('user get 1');
 });
 
 module.exports = app;

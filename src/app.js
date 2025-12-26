9a281cbb217ea59c3cdc53c66779f0c543984a47
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-12-26 22:09:23 +0530

    playing with multiple route handler and next() function and error along with res.send()

--- src/app.js
+++ src/app.js
@@ -2,58 +2,26 @@ const express = require('express');
 
 const app = express();
 
-// Matches: /ab, /abc
-app.get(/^\/ab?c$/, (req, res) => {
-  res.send('Pattern: ab?c');
-});
-
-// Matches: /abc, /abbc, /abbbc, /abbbbbbc
-app.get(/^\/ab+c$/, (req, res) => {
-  res.send("Pattern: ab+c");
-});
-
-// Matches: /abcd, /abxcd, /ab123cd, /abRANDOMcd
-app.get(/^\/ab.*cd$/, (req, res) => {
-  res.send("Pattern: ab*cd");
-});
-
-// Express string patterns treat * as a wildcard
-app.get('/ab*cd', (req, res) => {
-  res.send("Pattern: ab*cd");
-});
-
-// Matches: /abc, /abc, /abcbcbc (one or more 'bc')
-app.get(/^\/a(bc)+$/, (req, res) => {
-  res.send("Pattern: a(bc)+");
-});
-
-// Matches any route containing 'fly'
-app.get(/.*fly$/, (req, res) => {
-  res.send("Route contains 'fly'");
-});
-
-// Access Query Parameter
-app.get('/user', (req, res) => {
-  console.log(req.query);
-  res.send(`User ID: ${req.query.userId}, Name: ${req.query.name1}`);
-});
-
-// Route Parameter
-app.get('/user/:userId', (req, res) => {
-  console.log(req.params);
-  res.send(`userId: ${req.params.userId}`);
-});
-
-// Multiple Parameter
-app.get('/user/:userId/post/:postId', (req, res) => {
-  console.log(req.params);
-  res.send(`User: ${req.params.userId}, Post: ${req.params.postId}`);
-});
-
-// URL: /search?term=nodejs&page=2
-app.get('/search', (req, res) => {
-  const { term, page } = req.query;
-  res.send(`Searching for: ${term}, Page: ${page}`);
-});
+// Middleware
+app.use(
+  '/user',
+  [
+    (req, res, next) => {
+      console.log('handle route 1');
+      // res.send('1st response!');
+      next();
+    },
+    (req, res, next) => {
+      console.log('handle route 2');
+      // res.send('2nd response!');
+      next();
+    },
+  ],
+  (req, res) => {
+    console.log('handle route 3');
+    res.send('3rd response!');
+    next();
+  }
+);
 
 module.exports = app;

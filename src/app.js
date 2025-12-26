7eed2898bd85aae52c14cb87030e354a965931d1
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-12-26 19:39:55 +0530

    playing with route handlers for user and search endpoints

--- src/app.js
+++ src/app.js
@@ -2,16 +2,58 @@ const express = require('express');
 
 const app = express();
 
+// Matches: /ab, /abc
+app.get(/^\/ab?c$/, (req, res) => {
+  res.send('Pattern: ab?c');
+});
+
+// Matches: /abc, /abbc, /abbbc, /abbbbbbc
+app.get(/^\/ab+c$/, (req, res) => {
+  res.send("Pattern: ab+c");
+});
+
+// Matches: /abcd, /abxcd, /ab123cd, /abRANDOMcd
+app.get(/^\/ab.*cd$/, (req, res) => {
+  res.send("Pattern: ab*cd");
+});
+
+// Express string patterns treat * as a wildcard
+app.get('/ab*cd', (req, res) => {
+  res.send("Pattern: ab*cd");
+});
+
+// Matches: /abc, /abc, /abcbcbc (one or more 'bc')
+app.get(/^\/a(bc)+$/, (req, res) => {
+  res.send("Pattern: a(bc)+");
+});
+
+// Matches any route containing 'fly'
+app.get(/.*fly$/, (req, res) => {
+  res.send("Route contains 'fly'");
+});
+
+// Access Query Parameter
 app.get('/user', (req, res) => {
-  console.log(req.method);        // GET, POST, etc.
-  console.log(req.url);           // /user
-  console.log(req.params);        // Route parameters
-  console.log(req.query);         // Query string parameters
-  console.log(req.body);          // Request body (needs middleware)
-  console.log(req.headers);       // HTTP headers
-  console.log(req.cookies);       // Cookies (needs cookie-parser)
-  console.log(req.ip);            // Client IP address
-  res.send('user get 1');
+  console.log(req.query);
+  res.send(`User ID: ${req.query.userId}, Name: ${req.query.name1}`);
+});
+
+// Route Parameter
+app.get('/user/:userId', (req, res) => {
+  console.log(req.params);
+  res.send(`userId: ${req.params.userId}`);
+});
+
+// Multiple Parameter
+app.get('/user/:userId/post/:postId', (req, res) => {
+  console.log(req.params);
+  res.send(`User: ${req.params.userId}, Post: ${req.params.postId}`);
+});
+
+// URL: /search?term=nodejs&page=2
+app.get('/search', (req, res) => {
+  const { term, page } = req.query;
+  res.send(`Searching for: ${term}, Page: ${page}`);
 });
 
 module.exports = app;

22680432c5c49822984f8b34c834eb12d7ecf023
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-12-27 16:48:51 +0530

    wild card error handling and try catch

--- src/app.js
+++ src/app.js
@@ -21,4 +21,27 @@ app.delete('/admin/deleteAllData', (req, res) => {
   res.send('All Users Data Deleted');
 });
 
+
+
+// Error Handling
+app.get('/users', (req, res) => {
+  throw new Error('error nnn21');
+});
+
+// Error Handling using try catch
+app.get('/users', (req, res) => {
+  try {
+    throw new Error('error in try catch block');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Wild Card Error Handling
+app.use('/', (err, req, res, next) => {
+  if (err) {
+    res.status(500).send('something went wrong!');
+  }
+});
+
 module.exports = app;

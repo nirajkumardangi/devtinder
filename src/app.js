97b86f04398fe460f1a334eb2299920a4dc7479d
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-12-28 20:50:15 +0530

    feat: add MongoDB connection and user model, implement user creation endpoint
    - Added dotenv for environment variable management.
    - Implemented MongoDB connection logic in a new db.js file.
    - Created a User model with schema validation.
    - Updated app.js to include a new endpoint for creating users.
    - Refactored server.js to establish database connection before starting the server.

--- src/app.js
+++ src/app.js
@@ -1,47 +1,20 @@
 const express = require('express');
+const User = require('./models/User');
 
 const app = express();
-const { adminAuth, userAuth } = require('./middlewares/auth');
 
-// Middleware
+app.use(express.json());
 
-// Handle Auth Middleware for all GET, POST,...Request
-app.use('/admin', adminAuth);
-
-app.get('/user', userAuth, (req, res) => {
-  res.send('Fetching user data');
-});
-
-// Get all user data
-app.get('/admin/getAllData', (req, res) => {
-  res.send('All User Data Sends!');
-});
-
-app.delete('/admin/deleteAllData', (req, res) => {
-  res.send('All Users Data Deleted');
-});
-
-
-
-// Error Handling
-app.get('/users', (req, res) => {
-  throw new Error('error nnn21');
-});
-
-// Error Handling using try catch
-app.get('/users', (req, res) => {
+// CREATE: create a new user in db
+const createUser = async (req, res) => {
   try {
-    throw new Error('error in try catch block');
-  } catch (err) {
-    res.status(500).send(err.message);
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-});
+};
 
-// Wild Card Error Handling
-app.use('/', (err, req, res, next) => {
-  if (err) {
-    res.status(500).send('something went wrong!');
-  }
-});
+app.post('/createUser', createUser);
 
 module.exports = app;

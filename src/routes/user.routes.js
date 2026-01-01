00d77c34f8f8568568384d020dfa41569e082998
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2026-01-01 07:20:54 +0530

    feat: implement user routes and controller for CRUD operations

---
+++ src/routes/user.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const { createUser, getUser, updateUser, deleteUser } = require('../controllers/user.controller');
+
+router.post('/', createUser);
+router.get('/:id', getUser);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+module.exports = router;

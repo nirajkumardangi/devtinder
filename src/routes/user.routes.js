a911b111d4410ee40d6dfaf56986477e681d4d17
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2026-01-02 11:52:58 +0530

    Get user by email

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,16 +7,19 @@ const {
   updateUser,
   deleteUser,
   signup,
+  getUserByEmail,
 } = require('../controllers/user.controller');
 
 // AUTH ROUTES
 router.post('/signup', signup);
+
+// SEARCH ROUTES
+router.get('/email', getUserByEmail);
 
 // CRUD ROUTES
 router.post('/', createUser);
 router.get('/:id', getUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
-
 
 module.exports = router;

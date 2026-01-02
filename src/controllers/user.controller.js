e7564329e435375001e2be45bf9ebf1896e4cea2
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2026-01-02 11:52:58 +0530

    Get user by email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -68,3 +68,29 @@ exports.signup = async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 };
+
+// Get User by Email
+exports.getUserByEmail = async (req, res) => {
+  try {
+    const email = req.query.email;
+
+    if (!email) {
+      return res.status(400).json({ message: 'Email is required' });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      });
+    }
+
+    res.json({
+      message: 'User found',
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};

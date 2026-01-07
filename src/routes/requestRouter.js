429780da237cfac27446ada23d1f6afb8bf4684b
Niraj Dangi <niraj8825224435@gmail.com>
2026-01-07 20:52:55 +0530

    feat: implement connection request functionality with validation and user existence checks

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middlewares/authMiddleware");
-const { sendRequest } = require("../controllers/connectionRequestController");
+const { sendRequest } = require("../controllers/requestController");
 
 // ========== PROTECTED ROUTES (Auth Required) ==========
 router.use(authMiddleware);

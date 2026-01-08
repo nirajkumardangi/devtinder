6659bc1c2b9f8048c52f3998efce8895c9f3f7b4
Niraj Dangi <niraj8825224435@gmail.com>
2026-01-08 10:05:39 +0530

    feat: implement getFeed endpoint to retrieve users not connected to the logged-in user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,48 @@
 const ConnectionRequest = require("../models/ConnectionRequest");
+const User = require("../models/User");
 
+// Get user feed (users not connected)
+exports.getFeed = async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    // Find all connection requests involving logged in user
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    // Create set of user IDs to hide from feed
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    // Find users not in hideUsersFromFeed
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select("name")
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({
+      message: "Feed fetched successfully",
+      data: users,
+    });
+  } catch (err) {
+    res.status(400).json({ message: "Internal server error" });
+  }
+};
+
+// Get all the connections for the loggedIn user
 exports.getConnections = async (req, res) => {
   try {
     const loggedInUser = req.user;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,10 +5,12 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const {
   getConnectionRequests,
   getConnections,
+  getFeed
 } = require("../controllers/userController");
 
 // User APIs:
 router.get("/requests/received", authMiddleware, getConnectionRequests);
 router.get("/connections", authMiddleware, getConnections);
+router.get("/feed", authMiddleware, getFeed);
 
 module.exports = router;
